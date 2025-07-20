06a277d29229239caad5d98453cd8aa80c38473a
juan <juagomezve@unal.edu.co>
2025-07-20 09:00:07 -0500

    actualización de cambios

--- control_bomba/cores/hc_sr04/perip_hc_sr04.v
+++ control_bomba/cores/hc_sr04/perip_hc_sr04.v
@@ -1,41 +1,39 @@
-module perip_hc_sr04(   
+module perip_hc_sr04 (
     input clk,
     input rst,
     input [31:0] d_in,
     input cs,
-    input [4:0] addr,
+    input [31:0] addr,  // el tamaño de addr es de 32 bits
     input rd,
     input wr,
     output reg [31:0] d_out,
     input echo,
-    output reg trigger,
-  );
+    output trigger  // trigger ya está definido como puerto
+);
 
-  reg [15:0] distance;
-  localparam integer GETDISTANCE = 5'h01; // en c GETDISTANCE 0x01 
-//leer registros
+  localparam integer GETDISTANCE = 5'h01;  // Definir en C -> #define  GET_DISTANCE 0x01
+  //leer registros
   always @(posedge clk) begin
-  if (rst) begin
-  distance <=0;
-  end 
-  else begin
-    if (cs && rd) begin
-      if (addr[4:0]==GETDISTANCE) begin
-        distance <= {d_out[0:15],16'b0};
+    if (rst) begin
+      d_out <= 0;
+    end else begin
+      if (cs && rd) begin  // Si hay una solicitud de lectura
+        if (addr[4:0] == GETDISTANCE) begin  // Si addr corresponde a GETDISTANCE
+          d_out <= {16'b0, wr_distance};  // Completar el bus con bits en 0
         end
       end
     end
-    
-    end
-//
 
- hc_sr04 hc_sr04_1 (
-  .clk(clk),
-  .echo(echo),
-  .trigger(trigger),
-  .distance(d_out),
-  .rst(rst),
-);
+  end
+
+  wire [15:0] wr_distance;  // Es solo un cable
+  hc_sr04 hc_sr04_1 (
+      .clk(clk),
+      .echo(echo),
+      .trigger(trigger),
+      .distance(wr_distance),  // 16bits
+      .rst(rst),
+  );
 
 endmodule
   
